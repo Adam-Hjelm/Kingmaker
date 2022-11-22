d78d140cd1907088392b5b4ab484daa1cca82770
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2022-11-22 11:11:23 +0100

    Added healtbar and a basic healthsystem

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    public Image healthBar;
+
+    public int currentHealth;
+    public int maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.fillAmount = maxHealth;
+        maxHealth = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(25);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
+    }
+}
