474d61f36c6abe0229c6bddb183ab9d9cc3b00f0
Kevin Holmgren <113012348+KvartzCode@users.noreply.github.com>
2022-11-25 13:21:56 +0100

    GameManager kinda works

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -28,9 +28,14 @@ public class GameManager : MonoBehaviour
     public const int maxScoreToWin = 3; // placeholder value
     public const int maxNumberOfRounds = 7; // placeholder value
 
-    List<PlayerInstance> players = new List<PlayerInstance>();
+    [SerializeField] List<PlayerInstance> players = new List<PlayerInstance>();
     int currentRound = 0;
     [SerializeField] CanvasHandler canvasHandler;
+
+    [SerializeField] Transform playerPos1;
+    [SerializeField] Transform playerPos2;
+    [SerializeField] Transform playerPos3;
+    [SerializeField] Transform playerPos4;
 
 
     void Awake()
@@ -39,7 +44,7 @@ public class GameManager : MonoBehaviour
 
         if (_instance != null && _instance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
         else
         {
@@ -59,6 +64,11 @@ public class GameManager : MonoBehaviour
 
     public void AddPlayer(int playerNumber, GameObject playerObject)
     {
+        Debug.Log("added player: " + playerNumber);
+
+        if (players.Count >= 4)
+            return;
+
         if (!players.Any(p => p.ID == playerNumber))
         {
             players.Add(new PlayerInstance
@@ -70,6 +80,16 @@ public class GameManager : MonoBehaviour
                 isAlive = true
             });
         }
+    }
+
+    public void RemovePlayer(int playerNumber)
+    {
+        Debug.Log("removed player: " + playerNumber);
+
+        PlayerInstance player = players.FirstOrDefault(p => p.ID == playerNumber);
+
+        if (player != null)
+            players.Remove(player);
     }
 
     private string GetPlayerName(int playerNumber)
@@ -88,22 +108,14 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void RemovePlayer(int playerNumber)
-    {
-        PlayerInstance player = players.FirstOrDefault(p => p.ID == playerNumber);
-
-        if (player != null)
-        {
-            players.Remove(player);
-        }
-    }
-
     public void KillPlayer(int playerNumber)
     {
         var player = players.FirstOrDefault(p => p.ID == playerNumber);
+
         if (player != null)
         {
             player.isAlive = false;
+            player.gameObject.SetActive(false);
             CheckWin(player);
         }
     }
@@ -113,13 +125,11 @@ public class GameManager : MonoBehaviour
         var livingPlayers = players.Where(p => p.isAlive).ToList();
 
         if (livingPlayers.Count() == 1)
-        {
             livingPlayers[0].score++;
-        }
         else if (livingPlayers.Count() < 1)
-        {
             lastKilledPlayer.score++;
-        }
+        else
+            return; // do this to avoid the win condition check below
 
         var leadingPlayer = players.OrderBy(p => p.score).FirstOrDefault();
 
@@ -132,9 +142,53 @@ public class GameManager : MonoBehaviour
     private void HandleNewRound()
     {
         currentRound++;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        canvasHandler.RoundText = $"Round: {currentRound}";
+
+        ResetScene();
+
+        //foreach (var player in players)
+        //{
+        //    player.isAlive = true;
+        //    player.gameObject.SetActive(true);
+        //}
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //canvasHandler.RoundText = $"Round: {currentRound}";
         //TODO: go to upgrade screen
+    }
+
+    /// <summary>
+    /// Sets all gameobjects in the scene to their initial state. Excluding objects like GameManager that keeps score and stats.
+    /// </summary>
+    public void ResetScene()
+    {
+        canvasHandler.RoundText = $"Round: {currentRound}";
+
+        //TODO: player positions
+
+        foreach (var player in players)
+        {
+            switch (player.ID)
+            {
+                case 1:
+                    player.gameObject.transform.position = playerPos1.position;
+                    break;
+                case 2:
+                    player.gameObject.transform.position = playerPos2.position;
+                    break;
+                case 3:
+                    player.gameObject.transform.position = playerPos3.position;
+                    break;
+                case 4:
+                default:
+                    player.gameObject.transform.position = playerPos4.position;
+                    break;
+            }
+
+            player.isAlive = true;
+            player.gameObject.SetActive(true);
+        }
+
+        //TODO: change state of UI
     }
 
     private void HandleWin(PlayerInstance winningPlayer)
@@ -145,13 +199,15 @@ public class GameManager : MonoBehaviour
     public void StartNewGame()
     {
         currentRound = 0;
+
         foreach (var player in players)
         {
             player.score = 0;
             player.isAlive = true;
             player.gameObject.SetActive(true);
         }
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void GoToMainMenu()
@@ -166,7 +222,6 @@ public class GameManager : MonoBehaviour
 #else
         Application.Quit();
 #endif
-
     }
 
 
