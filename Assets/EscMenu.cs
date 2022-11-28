3251c8cd8d4383c97a6de6a9d331564c2c3b3f72
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2022-11-28 11:20:07 +0100

    Dotween

---
+++ Assets/EscMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+
+public class EscMenu : MonoBehaviour
+{
+    public float endPosY;
+    private float startPosY;
+    public bool hidden;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && hidden == true)
+        {
+            transform.DOMoveY(endPosY, 1).OnComplete(Complete).SetEase(Ease.OutBounce);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && hidden == false)
+        {
+            transform.DOMoveY(startPosY, 1).OnComplete(Complete).SetEase(Ease.InBounce);
+        }
+    }
+
+    void Complete()
+    {
+        hidden = !hidden;
+    }
+    public void Resume()
+    {
+        transform.DOMoveY(startPosY, 1).OnComplete(Complete).SetEase(Ease.InBounce);
+        Debug.Log("pressed");
+    }
+}
