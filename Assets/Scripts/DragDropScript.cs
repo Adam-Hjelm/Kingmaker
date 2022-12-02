9be913c179948276ba16be3a4d939849ffe42924
Adam Hjelm <adam.hjelm.special@gmail.com>
2022-12-02 11:37:29 +0100

    Updated Upgrade & Game Scene BUILD VERSION
    This is the build version that was used for the first playtest

--- Assets/Scripts/DragDropScript.cs
+++ Assets/Scripts/DragDropScript.cs
@@ -9,7 +9,11 @@ public class DragDropScript : MonoBehaviour
     public float dragSpeed;
     public bool colliding = false;
 
+    public Color pickedUpColor;
+    public Color startColor;
+
     public GameObject draggedObject;
+
     //public Collider2D draggedCollider;
     public State currentState;
 
@@ -22,6 +26,7 @@ public class DragDropScript : MonoBehaviour
     void Start()
     {
         currentState = State.Waiting;
+        //startColor = gameObject.GetComponent<SpriteRenderer>().color;
     }
 
     void Update()
@@ -42,10 +47,26 @@ public class DragDropScript : MonoBehaviour
 
     private void MoveDragNDropCursor()
     {
+
         Vector2 LDirection = new Vector2(Input.GetAxisRaw($"LHorizontal {playerNumber}"), Input.GetAxisRaw($"LVertical {playerNumber}"));
         float inputMagnitude = Mathf.Clamp01(LDirection.magnitude);
         LDirection.Normalize();
         transform.Translate(LDirection * dragSpeed * inputMagnitude * Time.fixedDeltaTime, Space.World);
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+
+        if (transform.position.x < pos.x || transform.position.y < pos.y)
+        {
+            transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+
+        if (-transform.position.x < -pos.x || -transform.position.y < -pos.y)
+        {
+            transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+
     }
 
     private void DragObject()
@@ -54,6 +75,8 @@ public class DragDropScript : MonoBehaviour
         gameObject.GetComponent<Collider2D>().enabled = false;
         draggedObject.transform.SetParent(gameObject.transform);
         currentState = State.Dragging;
+
+        draggedObject.GetComponent<SpriteRenderer>().color = pickedUpColor;
     }
 
     private void DropObject()
@@ -63,6 +86,8 @@ public class DragDropScript : MonoBehaviour
         draggedObject.transform.SetParent(null);
 
         colliding = false;
+
+        draggedObject.GetComponent<SpriteRenderer>().color = startColor;
         currentState = State.Waiting;
     }
 
