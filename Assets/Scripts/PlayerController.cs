dabf165a6bbc1e36dcda2dd75de74daa6c631b2f
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2022-12-08 11:55:14 +0100

    quick commit (sorry Adam)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,168 +2,170 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 using UnityEngine.UIElements.Experimental;
 
 public class PlayerController : MonoBehaviour
 {
-    public int playerNumber = 0;
+	public int playerNumber = 0;
 
-    [Header("Stats")]
-    public float moveSpeed = 3.5f;
-    public int maxHealth = 100;
-    public int bulletDamage = 25;
-    public float fireRate = 0.7f;
-    public int currentHealth = 100;
-    public float healthBarDegradeModifier;
+	[Header("Stats")]
+	public float moveSpeed = 3.5f;
+	public int maxHealth = 100;
+	public int bulletDamage = 25;
+	public float fireRate = 0.7f;
+	public int currentHealth = 100;
+	public float healthBarDegradeModifier;
 
-    [Header("Materials")]
-    public Material flashMaterial;
-    public Material defaultMaterial;
+	[Header("Materials")]
+	public Material flashMaterial;
+	public Material defaultMaterial;
 
-    [Header("Flash")]
-    public float flashDuration = 0.1f;
-    private Coroutine flashRoutine;
+	[Header("Flash")]
+	public float flashDuration = 0.1f;
+	private Coroutine flashRoutine;
 
-    [SerializeField] Image healthBar;
-    [SerializeField] Image healthBarBackdrop;
+	[SerializeField] Image healthBar;
+	[SerializeField] Image healthBarBackdrop;
 
-    public SpriteRenderer spriteRenderer;
+	public SpriteRenderer spriteRenderer;
 
-    CameraShake shake;
-    //public DragDropScript dragDropPlayer;
+	CameraShake shake;
+	//public DragDropScript dragDropPlayer;
 
-    public AudioClip Explosion;
-    public AudioSource Source;
+	public AudioClip Explosion;
+	public AudioSource Source;
+	public UpgradeController upgradeController;
+	public GameObject DeathPrefab;
+	public Transform DeathAnimationPoint;
 
-    public GameObject DeathPrefab;
-    public Transform DeathAnimationPoint;
-
-    public bool dashing;
-    public bool isBlocking;
-
-
-    void Awake()
-    {
-        PlayerController[] playerInstances = FindObjectsOfType<PlayerController>();
-
-        if (playerInstances.Length > 4)
-        {
-            Destroy(gameObject);
-        }
-
-        //DontDestroyOnLoad(gameObject);
-        currentHealth = maxHealth /*+ addedHealth*/;
-
-    }
-
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        //originalMaterial = spriteRenderer.material; aja baja dumma bug
-        spriteRenderer.material = defaultMaterial;
-        shake = Camera.main.GetComponent<CameraShake>();
-
-        healthBar = GameObject.FindWithTag($"Player{playerNumber}HealthBar").GetComponent<Image>();
-        healthBarBackdrop = GameObject.Find($"p{playerNumber}HealthbarBackdrop").GetComponent<Image>();
-
-        //healthBarBackdrop.fillAmount = 1; 
-    }
-
-    private void Update()
-    {
-        if (healthBarBackdrop != null)
-        {
-            if (healthBarBackdrop.fillAmount > healthBar.fillAmount)
-            {
-                healthBarBackdrop.fillAmount -= Time.deltaTime * healthBarDegradeModifier;
-            }
-
-            if (healthBarBackdrop.fillAmount < healthBar.fillAmount)
-            {
-                healthBarBackdrop.fillAmount = healthBar.fillAmount;
-            }
-        }
+	public bool dashing;
+	public bool isBlocking;
 
 
-    }
+	void Awake()
+	{
+		PlayerController[] playerInstances = FindObjectsOfType<PlayerController>();
 
-    private void PlayerTakeDmg(int dmg)
-    {
-        Debug.Log("take damage");
-        currentHealth -= dmg;
-        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
+		if (playerInstances.Length > 4)
+		{
+			Destroy(gameObject);
+		}
 
-        if (currentHealth <= 0 && GameManager.Instance != null)
-        {
-            StopCoroutine(flashRoutine);
-            spriteRenderer.material = defaultMaterial;
-            healthBarBackdrop.fillAmount = healthBar.fillAmount;
-            GameObject newDeath = Instantiate(DeathPrefab, DeathAnimationPoint.position, DeathAnimationPoint.rotation);
-            Destroy(newDeath, 1);
-            GameManager.Instance.KillPlayer(gameObject);
-            //ExplosionSound();
-            shake.start = true;
-        }
-    }
+		//DontDestroyOnLoad(gameObject);
+		currentHealth = maxHealth /*+ addedHealth*/;
+
+	}
+
+	void Start()
+	{
+		
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		//originalMaterial = spriteRenderer.material; aja baja dumma bug
+		spriteRenderer.material = defaultMaterial;
+		shake = Camera.main.GetComponent<CameraShake>();
+
+		healthBar = GameObject.FindWithTag($"Player{playerNumber}HealthBar").GetComponent<Image>();
+		healthBarBackdrop = GameObject.Find($"p{playerNumber}HealthbarBackdrop").GetComponent<Image>();
+
+		//healthBarBackdrop.fillAmount = 1; 
+	}
+
+	private void Update()
+	{
+		if (healthBarBackdrop != null)
+		{
+			if (healthBarBackdrop.fillAmount > healthBar.fillAmount)
+			{
+				healthBarBackdrop.fillAmount -= Time.deltaTime * healthBarDegradeModifier;
+			}
+
+			if (healthBarBackdrop.fillAmount < healthBar.fillAmount)
+			{
+				healthBarBackdrop.fillAmount = healthBar.fillAmount;
+			}
+		}
+
+
+	}
+
+	private void PlayerTakeDmg(int dmg)
+	{
+		Debug.Log("take damage");
+		currentHealth -= dmg;
+		healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
+
+		if (currentHealth <= 0 && GameManager.Instance != null)
+		{
+			StopCoroutine(flashRoutine);
+			spriteRenderer.material = defaultMaterial;
+			healthBarBackdrop.fillAmount = healthBar.fillAmount;
+			GameObject newDeath = Instantiate(DeathPrefab, DeathAnimationPoint.position, DeathAnimationPoint.rotation);
+			Destroy(newDeath, 1);
+			GameManager.Instance.KillPlayer(gameObject);
+			//ExplosionSound();
+			shake.start = true;
+		}
+	}
 
 
 
-    private void PlayerHeal(int healing)
-    {
-        currentHealth += healing;
-        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
-    }
+	private void PlayerHeal(int healing)
+	{
+		currentHealth += healing;
+		healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
+	}
 
-    public void SetPlayerHealthToMax()
-    {
-        currentHealth = maxHealth;
-        healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
-    }
+	public void SetPlayerHealthToMax()
+	{
+		currentHealth = maxHealth;
+		healthBar.fillAmount = (float)currentHealth / (float)maxHealth;
+	}
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("bullet") && !dashing || other.gameObject.CompareTag("bullet") && !isBlocking)
-        {
-            Flash();
-            Destroy(other.gameObject);
-            PlayerTakeDmg(other.gameObject.GetComponent<BulletScript>().bulletDamage);
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("bullet") && !dashing || other.gameObject.CompareTag("bullet") && !isBlocking)
+		{
+			Flash();
+			Destroy(other.gameObject);
+			PlayerTakeDmg(other.gameObject.GetComponent<BulletScript>().bulletDamage);
+		}
+	}
 
-    public void Flash()
-    {
-        if (flashRoutine != null)
-        {
-            StopCoroutine(FlashRoutine());
-        }
+	public void Flash()
+	{
+		if (flashRoutine != null)
+		{
+			StopCoroutine(FlashRoutine());
+		}
 
-        flashRoutine = StartCoroutine(FlashRoutine());
-    }
+		flashRoutine = StartCoroutine(FlashRoutine());
+	}
 
-    private IEnumerator FlashRoutine()
-    {
-        spriteRenderer.material = flashMaterial;
+	private IEnumerator FlashRoutine()
+	{
+		spriteRenderer.material = flashMaterial;
 
-        yield return new WaitForSeconds(flashDuration);
+		yield return new WaitForSeconds(flashDuration);
 
-        spriteRenderer.material = defaultMaterial;
+		spriteRenderer.material = defaultMaterial;
 
-        flashRoutine = null;
-    }
+		flashRoutine = null;
+	}
 
-    //public void OnMoveCursor(InputValue input)
-    //{
-    //    dragDropPlayer.OnMove(input.Get<Vector2>());
-    //}
+	//public void OnMoveCursor(InputValue input)
+	//{
+	//    dragDropPlayer.OnMove(input.Get<Vector2>());
+	//}
 
-    //public void OnFireCursor()
-    //{
-    //    dragDropPlayer.OnFire();
-    //}
+	//public void OnFireCursor()
+	//{
+	//    dragDropPlayer.OnFire();
+	//}
 
-    private void ExplosionSound()
-    {
-        Source.PlayOneShot(Explosion);
-    }
+	private void ExplosionSound()
+	{
+		Source.PlayOneShot(Explosion);
+	}
 }
--- Assets/Scripts/UpgradeController.cs
+++ Assets/Scripts/UpgradeController.cs
@@ -11,196 +11,201 @@ using UnityEngine.InputSystem.UI;
 
 public class UpgradeController : MonoBehaviour
 {
-    public Transform[] cardSpawnPos;
-    public Button[] playerButtons;
-    public List<Button> upgradeCardButtons = new List<Button>();
+	public Transform[] cardSpawnPos;
+	public Button[] playerButtons;
+	public List<Button> upgradeCardButtons = new List<Button>();
 
-    //public Button[] upgradeCardButtons;
-    public Button startUpgradeCard;
-    public Button currentlySelectedCard;
-    public Button chosenUpgradeCard;
-    public GameObject cardButtonPrefab;
-    public Canvas upgradeCanvas;
-    public TextMeshProUGUI playerChooseText;
+	//public Button[] upgradeCardButtons;
+	public Button startUpgradeCard;
+	public Button currentlySelectedCard;
+	public Button chosenUpgradeCard;
+	public GameObject cardButtonPrefab;
+	public Canvas upgradeCanvas;
+	public TextMeshProUGUI playerChooseText;
 
-    private UpgradeCardScript upgradeCardScript;
-    private PlayerController playerStats;
+	private UpgradeCardScript upgradeCardScript;
+	private PlayerController playerStats;
 
-    public int playerNumberToGiveStat;
-    public bool inPlayerButtons = false;
+	public int playerNumberToGiveStat;
+	public bool inPlayerButtons = false;
 
-    public Vector3 cardOffset;
-    public int playerToChooseCard = 1;
+	public Vector3 cardOffset;
+	public int playerToChooseCard = 1;
 
-    public MultiplayerEventSystem playerEventSys1;
-    public MultiplayerEventSystem playerEventSys2;
-    public MultiplayerEventSystem playerEventSys3;
-    public MultiplayerEventSystem playerEventSys4;
+	public MultiplayerEventSystem playerEventSys1;
+	public MultiplayerEventSystem playerEventSys2;
+	public MultiplayerEventSystem playerEventSys3;
+	public MultiplayerEventSystem playerEventSys4;
 
-    public MultiplayerEventSystem eventSysInUse;
+	public MultiplayerEventSystem eventSysInUse;
 
 
 	public void OnEnable()
-	{
-        eventSysInUse = playerEventSys1;
-        eventSysInUse.SetSelectedGameObject(startUpgradeCard.gameObject);
+	{ // kanske måste göra en null check ifall vi någonsin ska ha det att funka med mindre än 4 spelare
+
+		
+		eventSysInUse = playerEventSys1;
+		eventSysInUse.SetSelectedGameObject(startUpgradeCard.gameObject);
 	}
 
 	void Update()
-    {
-        if (eventSysInUse.currentSelectedGameObject == null)
-        {
-            currentlySelectedCard = null;
-            return;
-        }
-        else if (eventSysInUse.currentSelectedGameObject.CompareTag("Card"))
-        {
-            currentlySelectedCard = eventSysInUse.currentSelectedGameObject.GetComponent<Button>();
-        }
+	{
+		if (eventSysInUse.currentSelectedGameObject == null)
+		{
+			currentlySelectedCard = null;
+			return;
+		}
+		else if (eventSysInUse.currentSelectedGameObject.CompareTag("Card"))
+		{
+			currentlySelectedCard = eventSysInUse.currentSelectedGameObject.GetComponent<Button>();
+		}
 
-        if (inPlayerButtons && chosenUpgradeCard != null)
-        {
-            chosenUpgradeCard.gameObject.transform.position = eventSysInUse.currentSelectedGameObject.GetComponent<Transform>().position + cardOffset;
-        }
-    }
+		if (inPlayerButtons && chosenUpgradeCard != null)
+		{
+			chosenUpgradeCard.gameObject.transform.position = eventSysInUse.currentSelectedGameObject.GetComponent<Transform>().position + cardOffset;
+		}
+	}
 
-    public void UpgradePlayerStat()
-    {
-        CheckPlayerToGiveStats();
-        //playerStats = GameObject.FindGameObjectWithTag($"Player{playerNumberToGiveStat}").GetComponent<PlayerController>();
-        //upgradeCardScript = selectedUpgradeCard.GetComponent<UpgradeCardScript>();
+	public void UpgradePlayerStat()
+	{
+		CheckPlayerToGiveStats();
+		//playerStats = GameObject.FindGameObjectWithTag($"Player{playerNumberToGiveStat}").GetComponent<PlayerController>();
+		//upgradeCardScript = selectedUpgradeCard.GetComponent<UpgradeCardScript>();
 
-        //inPlayerButtons = false;
+		//inPlayerButtons = false;
 
-        //if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.HealthUp)
-        //{
-        //    playerStats.maxHealth += 100;
-        //}
+		//if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.HealthUp)
+		//{
+		//    playerStats.maxHealth += 100;
+		//}
 
-        //if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.DamageUp)
-        //{
-        //    playerStats.bulletDamage += 25;
-        //}
+		//if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.DamageUp)
+		//{
+		//    playerStats.bulletDamage += 25;
+		//}
 
-        //if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.SpeedUp)
-        //{
-        //    playerStats.moveSpeed += 2;
-        //}
+		//if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.SpeedUp)
+		//{
+		//    playerStats.moveSpeed += 2;
+		//}
 
-        //if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.FireRateUp)
-        //{
-        //    playerStats.fireRate += 0.15f;
-        //}
-        ////selectedUpgradeCard.GetComponent<SpriteRenderer>().enabled = false;
-        //upgradeCardButtons.Remove(selectedUpgradeCard);
+		//if (upgradeCardScript.currentCardType == UpgradeCardScript.CardType.FireRateUp)
+		//{
+		//    playerStats.fireRate += 0.15f;
+		//}
+		////selectedUpgradeCard.GetComponent<SpriteRenderer>().enabled = false;
+		//upgradeCardButtons.Remove(selectedUpgradeCard);
 
-        SpawnNewCards();
+		SpawnNewCards();
+		Debug.Log("not in player buttons!!!");
+		inPlayerButtons = false;
+	}
 
-        inPlayerButtons = false;
-    }
+	private void CheckPlayerToGiveStats()
+	{
+		string playerButtonName = EventSystem.current.currentSelectedGameObject.GetComponent<Button>().name;
 
-    private void CheckPlayerToGiveStats()
-    {
-        string playerButtonName = EventSystem.current.currentSelectedGameObject.GetComponent<Button>().name;
+		if (playerButtonName.Contains("1"))
+		{
+			playerNumberToGiveStat = 1;
+		}
 
-        if (playerButtonName.Contains("1"))
-        {
-            playerNumberToGiveStat = 1;
-        }
+		if (playerButtonName.Contains("2"))
+		{
+			playerNumberToGiveStat = 2;
+		}
 
-        if (playerButtonName.Contains("2"))
-        {
-            playerNumberToGiveStat = 2;
-        }
+		if (playerButtonName.Contains("3"))
+		{
+			playerNumberToGiveStat = 3;
+		}
 
-        if (playerButtonName.Contains("3"))
-        {
-            playerNumberToGiveStat = 3;
-        }
+		if (playerButtonName.Contains("4"))
+		{
+			playerNumberToGiveStat = 4;
+		}
+	}
 
-        if (playerButtonName.Contains("4"))
-        {
-            playerNumberToGiveStat = 4;
-        }
-    }
+	private void SpawnNewCards()
+	{
+		chosenUpgradeCard = null;
 
-    private void SpawnNewCards()
-    {
-        //Debug.Log("spawning next card..");
+		for (int i = 0; i < upgradeCardButtons.Count; i++)
+		{
+			if (upgradeCardButtons[i] != null)
+			{
+				Destroy(upgradeCardButtons[i].gameObject);
+			}
+		}
 
-        for (int i = 0; i < upgradeCardButtons.Count; i++)
-        {
-            if (upgradeCardButtons[i] != null)
-            {
-                Destroy(upgradeCardButtons[i].gameObject);
-            }
-        }
+		upgradeCardButtons = upgradeCardButtons.Where(item => item != null).ToList();
 
-        for (int i = 0; i < cardSpawnPos.Length; i++)
-        {
-            GameObject newButton = Instantiate(cardButtonPrefab, cardSpawnPos[i].position, Quaternion.identity, upgradeCanvas.transform);
+		for (int i = 0; i < cardSpawnPos.Length; i++)
+		{
+			var newButton = Instantiate(cardButtonPrefab, cardSpawnPos[i].position, Quaternion.identity, upgradeCanvas.transform).GetComponent<Button>();
 
-            newButton.GetComponent<Button>().onClick.AddListener(MoveToPlayerButtons);
-            startUpgradeCard = newButton.GetComponent<Button>();
-            //newButton.transform.SetParent(upgradeCanvas.transform, true);
+			newButton.onClick.AddListener(MoveToPlayerButtons);
+			//newButton.transform.SetParent(upgradeCanvas.transform, true);
+			startUpgradeCard = newButton;
 
-            upgradeCardButtons.Add(newButton.GetComponent<Button>());
-        }
+			upgradeCardButtons.Add(newButton.GetComponent<Button>());
+		}
 
-        CheckForNextPlayer();
-    }
+		//startUpgradeCard = upgradeCardButtons[0].GetComponent<Button>();
+		CheckForNextPlayer();
+	}
 
-    private void CheckForNextPlayer()
-    {
-        playerChooseText.text = $"Player {playerToChooseCard},Choose a Card";
-
+	private void CheckForNextPlayer()
+	{
 		switch (playerToChooseCard)
 		{
 			case 1:
-				eventSysInUse = playerEventSys1;
-                playerToChooseCard = 2;
-                break;
-			case 2:
 				eventSysInUse = playerEventSys2;
-                playerToChooseCard = 3;
-                break;
-			case 3:
+				playerToChooseCard = 2;
+				break;
+			case 2:
 				eventSysInUse = playerEventSys3;
-                playerToChooseCard = 4;
-                break;
-            default:
-			case 4:
+				playerToChooseCard = 3;
+				break;
+			case 3:
 				eventSysInUse = playerEventSys4;
-                playerToChooseCard = 1;
-                Invoke(nameof(FinishedUpgrade), 3);
+				playerToChooseCard = 4;
+				break;
+			default:
+			case 4:
+				eventSysInUse = playerEventSys1;
+				playerToChooseCard = 1;
+				Invoke(nameof(FinishedUpgrade), 3);
 				break;
 		}
 
+		playerChooseText.text = $"Player {playerToChooseCard},Choose a Card";
+
 		playerEventSys1.SetSelectedGameObject(null);
-        playerEventSys2.SetSelectedGameObject(null);
-        playerEventSys3.SetSelectedGameObject(null);
-        playerEventSys4.SetSelectedGameObject(null);
+		playerEventSys2.SetSelectedGameObject(null);
+		playerEventSys3.SetSelectedGameObject(null);
+		playerEventSys4.SetSelectedGameObject(null);
 
-        eventSysInUse.SetSelectedGameObject(startUpgradeCard.gameObject);
-    }
+		eventSysInUse.SetSelectedGameObject(startUpgradeCard.gameObject);
+	}
 
-    void FinishedUpgrade()
-    {
-        GameManager.Instance.FinishedUpgrade();
-    }
+	void FinishedUpgrade()
+	{
+		GameManager.Instance.FinishedUpgrade();
+	}
 
-    public void MoveToPlayerButtons()
-    {
-        inPlayerButtons = true;
-        upgradeCardButtons = upgradeCardButtons.Where(item => item != null).ToList();
+	public void MoveToPlayerButtons()
+	{
+		inPlayerButtons = true;
+		playerButtons[playerToChooseCard].onClick.RemoveAllListeners(); // Här kan vi graya ut knappen så att spelaren inte tror att den kan interagera med sig själv
 
-        if (chosenUpgradeCard != null)
-        {
-            // Here you can make the card do some cool animations before it goes away and has been given to the player
-            chosenUpgradeCard.gameObject.SetActive(false);
-        }
-        chosenUpgradeCard = currentlySelectedCard;
+		if (chosenUpgradeCard != null)
+		{
+			// Here you can make the card do some cool animations before it goes away and has been given to the player
+			chosenUpgradeCard.gameObject.SetActive(false);
+		}
+		chosenUpgradeCard = currentlySelectedCard;
 
-        EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(playerButtons[0].gameObject);
-    }
+		EventSystem.current.GetComponent<EventSystem>().SetSelectedGameObject(playerButtons[0].gameObject);
+	}
 }
