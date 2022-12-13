8060ff9d122ebb662c9c07acdc95ed7bf6912fed
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2022-12-13 12:57:11 +0100

    Changed how selection screen works

--- Assets/Scripts/SelectionScreen.cs
+++ Assets/Scripts/SelectionScreen.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -12,64 +13,7 @@ using UnityEngine.UI;
 
 public class SelectionScreen : MonoBehaviour
 {
-    #region DO NOT TOUCH!
-    // WARNING!
-    // This part is taken from PlayerInputManager.cs source code and it works. If you get errors from here something else is wrong.
-    // Contact Kevin if you get stuck here.
-    // DO NOT MODIFY THIS REGION!
-
-
-    [SerializeField] internal bool m_AllowJoining = true;
-
-    [NonSerialized] private bool m_JoinActionDelegateHooked;
-    [NonSerialized] private Action<InputAction.CallbackContext> m_JoinActionDelegate;
-    [NonSerialized] private bool m_LeaveActionDelegateHooked;
-    [NonSerialized] private Action<InputAction.CallbackContext> m_LeaveActionDelegate;
-
-    [SerializeField] internal InputActionProperty m_JoinAction;
-    [SerializeField] internal InputActionProperty m_LeaveAction;
-
-    private void DoOnEnable()
-    {
-        m_JoinActionDelegate = JoinPlayer;
-        m_LeaveActionDelegate = RemovePlayer;
-
-        RemoveActionDelegates();
-
-        m_JoinAction.action.performed += m_JoinActionDelegate;
-        m_JoinActionDelegateHooked = true;
-        m_JoinAction.action.Enable();
-
-        m_LeaveAction.action.performed += m_LeaveActionDelegate;
-        m_LeaveActionDelegateHooked = true;
-        m_LeaveAction.action.Enable();
-    }
-
-    private void DoOnDisable()
-    {
-        RemoveActionDelegates();
-    }
-
-    private void RemoveActionDelegates()
-    {
-        if (m_JoinActionDelegateHooked)
-        {
-            m_JoinAction.action.performed -= m_JoinActionDelegate;
-            m_JoinActionDelegateHooked = false;
-            m_JoinAction.action?.Disable();
-        }
-
-        if (m_LeaveActionDelegateHooked)
-        {
-            m_LeaveAction.action.performed -= m_LeaveActionDelegate;
-            m_LeaveActionDelegateHooked = false;
-            m_LeaveAction.action?.Disable();
-        }
-    }
-
-    #endregion
-
-
+    [SerializeField] private bool allowJoining = true;
     public string gameScene = "GameScene";
 
     [SerializeField] int totalPlayers = 0;
@@ -90,140 +34,123 @@ public class SelectionScreen : MonoBehaviour
     [SerializeField] GameObject playerCard4;
 
     [SerializeField] List<Sprite> playerSprites = new List<Sprite>();
+    
 
-
-    private void Awake()
+	private void Awake()
     {
         DontDestroyOnLoad(this);
     }
 
 	private void OnEnable()
 	{
-        DoOnEnable(); // Do not remove!
-        
         SceneManager.sceneLoaded += GameSceneLoaded;
         Debug.Log("sceneloaded delegate is set to gamesceneloaded method!");
     }
 
     private void OnDisable()
     {
-        DoOnDisable(); // Do not remove!
-
         SceneManager.sceneLoaded -= GameSceneLoaded;
+        if (PlayerInputManager.instance != null)
+            PlayerInputManager.instance.onPlayerJoined -= OnPlayerJoined;
     }
 
-	private void Update()
-	{
-		if ((Gamepad.current != null && Gamepad.current.startButton.isPressed))
-		//(Joystick.current != null && Joystick.current.))
-		{
-			Debug.Log("start was pressed");
-			//SceneManager.LoadScene(gameScene);
-			StartGame();
-		}
-	}
-
-
-
-	public void JoinPlayer(InputAction.CallbackContext context)
+    void Start()
     {
-        Debug.Log("JoinPlayer method called");
+        if (PlayerInputManager.instance != null)
+            PlayerInputManager.instance.onPlayerJoined += OnPlayerJoined;
+    }
 
-        if (!m_AllowJoining || totalPlayers >= maxAllowedPlayers)
+    private void OnPlayerJoined(PlayerInput playerInput)
+    {
+        Debug.Log("OnPlayerJoined() called!");
+        //JoinPlayer(playerInput.gameObject);
+        var device = playerInput.devices.FirstOrDefault();
+        int ID = (int)playerInput.user.id;
+		List<int> ids = players.Select(p => p.playerID).ToList();
+
+        if (!allowJoining || totalPlayers >= maxAllowedPlayers)
             return;
-
-        var device = context.control.device;
 
         if (players.Any(p => p.device == device))
             return;
 
-        //if (PlayerInput.FindFirstPairedToDevice(device) != null)
-        //    return;
-
-        int id = 1;
-        List<int> ids = players.Select(p => p.playerID).ToList();
-
-        if (!ids.Any(i => i == 1))
-            id = 1;
-        else if (!ids.Any(i => i == 2))
-            id = 2;
-        else if (!ids.Any(i => i == 3))
-            id = 3;
-        else if (!ids.Any(i => i == 4))
-            id = 4;
-        else
+        if (ids.Any(i => i == ID))
+		{
+            Debug.LogError("ID already exists!");
             return;
+		}
 
-        Debug.Log(id);
+		var playerCard = playerInput.gameObject;
+        playerCard.transform.SetParent(canvasObject.transform, false);
 
-        GameObject playerCard;
-
-        if (playerCard1 == null)
-        {
-            playerCard = Instantiate(playerCardPrefab, spawnPos1.position, spawnPos1.rotation, canvasObject.transform);
-            playerCard.transform.Find("Character").GetComponent<Image>().sprite = playerSprites[0];
-            playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>().text = "Red Player";
+		if (playerCard1 == null)
+		{
+            SetPositionOfPlayerCard(playerCard, spawnPos1, playerSprites[0], "Red Player");
             playerCard1 = playerCard;
         }
-        else if (playerCard2 == null)
-        {
-            playerCard = Instantiate(playerCardPrefab, spawnPos2.position, spawnPos2.rotation, canvasObject.transform);
-            playerCard.transform.Find("Character").GetComponent<Image>().sprite = playerSprites[1];
-            playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>().text = "Blue Player";
+		else if (playerCard2 == null)
+		{
+            SetPositionOfPlayerCard(playerCard, spawnPos2, playerSprites[1], "Blue Player");
             playerCard2 = playerCard;
         }
         else if (playerCard3 == null)
-        {
-            playerCard = Instantiate(playerCardPrefab, spawnPos3.position, spawnPos3.rotation, canvasObject.transform);
-            playerCard.transform.Find("Character").GetComponent<Image>().sprite = playerSprites[2];
-            playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>().text = "Green Player";
+		{
+			SetPositionOfPlayerCard(playerCard, spawnPos3, playerSprites[2], "Green Player");
             playerCard3 = playerCard;
-        }
-        else if (playerCard4 == null)
-        {
-            playerCard = Instantiate(playerCardPrefab, spawnPos4.position, spawnPos4.rotation, canvasObject.transform);
-            playerCard.transform.Find("Character").GetComponent<Image>().sprite = playerSprites[3];
-            playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>().text = "Pink Player";
+		}
+		else if (playerCard4 == null)
+		{
+            
+            SetPositionOfPlayerCard(playerCard, spawnPos4, playerSprites[3], "Pink Player");
             playerCard4 = playerCard;
-        }
-        else
-        {
-            Debug.LogError("JoinPlayer method called but player count is already at maximum");
-            return;
-        }
+		}
+		else
+		{
+			Debug.LogError("JoinPlayer method called but player count is already at maximum");
+			return;
+		}
 
-        //playerCard.SetActive(true);
-
-        //TextMeshProUGUI playerName = playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>();
-        //TextMeshProUGUI playerInfo = playerCard.transform.Find("user info").GetComponent<TextMeshProUGUI>();
-
-        
-
-        //playerName.text = device.displayName;
-        //playerInfo.text = $"name: '{device.name}'\ndisplayName: '{device.displayName}'\ndeviceId: '{device.deviceId}'\nnative: '{device.native}'";
-
-        var player = new PlayerObject
+		var player = new PlayerObject
         {
             device = device,
             card = playerCard,
-            playerID = id
+            playerID = ID
         };
 
         players.Add(player);
         totalPlayers++;
 
-        if (totalPlayers >= maxAllowedPlayers)
-            m_AllowJoining = false;
+		if (totalPlayers >= maxAllowedPlayers)
+        {
+			allowJoining = false;
+            PlayerInputManager.instance.DisableJoining();
+        }
     }
 
-    public void RemovePlayer(InputAction.CallbackContext context)
+    private void OnPlayerLeft(PlayerInput playerInput)
     {
+        Debug.Log("OnPlayerLeft() called!");
+    }
+
+    public void PlayerReady(InputAction input)
+    {
+        Debug.Log("OnReady() called!");
+    }
+
+    private void SetPositionOfPlayerCard(GameObject playerCard, Transform spawn, Sprite sprite, string playerName)
+	{
+        playerCard.transform.SetPositionAndRotation(spawn.position, spawn.rotation);
+        playerCard.transform.Find("Character").GetComponent<Image>().sprite = sprite;
+        playerCard.transform.Find("user text").GetComponent<TextMeshProUGUI>().text = playerName;
+    }
+
+	public void RemovePlayer(InputDevice device)
+	{
 		Debug.Log("RemovePlayer method called");
 
 		if (totalPlayers <= 0)
 			return;
 
-		var device = context.control.device;
 		var player = players.FirstOrDefault(p => p.device == device);
 
 		if (player == null)
@@ -231,16 +158,30 @@ public class SelectionScreen : MonoBehaviour
 
 		players.Remove(player);
 		Destroy(player.card);
-		totalPlayers--;
-        Debug.Log("Player was removed!");
 
-        if (totalPlayers < maxAllowedPlayers)
-			m_AllowJoining = true;
+		totalPlayers--;
+		Debug.Log("Player was removed!");
+
+		if (totalPlayers < maxAllowedPlayers)
+			allowJoining = true;
 	}
 
     public void StartGame()
     {
         Debug.Log("pressed Start Game");
+        if (PlayerInputManager.instance != null)
+            PlayerInputManager.instance.onPlayerJoined -= OnPlayerJoined;
+        StartCoroutine(DestroyPlayersAndInputManager());
+    }
+
+    private IEnumerator DestroyPlayersAndInputManager()
+    {
+        foreach (var player in players)
+        {
+            Destroy(player.card);
+            yield return null;
+        }
+        yield return null;
         SceneManager.LoadScene(gameScene);
     }
 
@@ -248,21 +189,23 @@ public class SelectionScreen : MonoBehaviour
     {
         Debug.Log("OnSceneLoaded: " + scene.name);
         Debug.Log("LoadSceneMode: " + mode);
-
         if (scene.name == gameScene)
+        {
             StartCoroutine(InstantiatePlayers());
+        }
     }
 
     private IEnumerator InstantiatePlayers()
     {
-        yield return 0;
+        yield return null;
+        yield return null;
+        yield return null;
 
-        var pim = PlayerInputManager.instance;
         players = players.OrderBy(p => p.playerID).ToList();
 
         foreach (var player in players)
         {
-            pim.JoinPlayer(playerIndex: player.playerID, pairWithDevice: player.device);
+            PlayerInputManager.instance.JoinPlayer(playerIndex: player.playerID, pairWithDevice: player.device);
         }
 
         Destroy(gameObject);
---
+++ Assets/Scripts/SelectionScreenPlayerCard.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SelectionScreenPlayerCard : MonoBehaviour
+{
+    InputDevice device;
+    bool isReady = false;
+
+    [SerializeField] GameObject readyBtn;
+    [SerializeField] GameObject notReadyBtn;
+    [SerializeField] PlayerInput playerInput;
+
+    
+    public void Initialize(InputDevice device)
+	{
+        this.device = device;
+        //var test = new PlayerInput();
+        //test.device
+        Debug.Log(device.valueType.ToString());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (device != null)
+		{
+            //if (device.valueType)
+		}
+    }
+
+    void OnReady()
+	{
+        isReady = !isReady;
+        Debug.Log("on ready");
+        if (isReady)
+		{
+            readyBtn.SetActive(true);
+            notReadyBtn.SetActive(false);
+		}
+        else
+        {
+            readyBtn.SetActive(false);
+            notReadyBtn.SetActive(true);
+        }
+    }
+}
