d6b6383ae852939c0e91a6184c159d1577bf1fd9
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2022-12-19 14:33:42 +0100

    Fixed win screen scales and changed how it works

--- Assets/Scripts/winningScreen.cs
+++ Assets/Scripts/winningScreen.cs
@@ -1,34 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class winningScreen : MonoBehaviour
+public class WinningScreen : MonoBehaviour
 {
-
-    public GameObject characterSprite;
+    public Image characterSprite;
     public GameObject crownSprite;
     public GameObject lightRay;
     public GameObject shadow;
-    public GameObject text;
+    //public GameObject text;
     //public GameObject buttons;
 
     private void OnEnable()
     {
         StartCoroutine(DoCode());
-
     }
 
     IEnumerator DoCode()
     {
         Debug.Log("PrintOnEnable: script was enabled");
-        characterSprite.SetActive(true);
+        characterSprite.gameObject.SetActive(true);
         yield return new WaitForSeconds(1);
         crownSprite.SetActive(true);
         yield return new WaitForSeconds(.7f);
         lightRay.SetActive(true);
         //shadow.SetActive(true);
         yield return new WaitForSeconds(.5f);
-        text.SetActive(true);
+        //text.SetActive(true);
         //buttons.SetActive(true);
         yield return null;
     }
