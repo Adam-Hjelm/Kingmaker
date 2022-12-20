4963160ee3ee3282457437fb66131495ab11de75
EmilCarlssonArt <118523364+EmilCarlssonArt@users.noreply.github.com>
2022-12-20 11:38:06 +0100

    Finns nu ett DoTween Punch script, atm bara applicerat på Selection Menyn

---
+++ Assets/Scripts/PunchScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PunchScript : MonoBehaviour
+{
+
+    public Vector3 punch =new Vector3(0.5f,0.5f,0.5f);
+    public float duration = 0.5f;
+    public int vibrato = 5;
+    public float elasticity = 2;
+
+
+    // Start is called before the first frame update
+    public void OnEnable()
+    {
+        //transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.5f, 5, 2);
+        transform.DOPunchScale(punch, duration, vibrato, elasticity);
+    }
+}
