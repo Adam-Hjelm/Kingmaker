5c47352ec0338557d023593ea087a9ba21858e2b
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2023-01-02 15:19:49 +0100

    Added AudioManager.cs example

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("GameManager instance is null!");
+
+            return _instance;
+        }
+    }
+
+
+    public AudioClip fireballSound1;
+    public AudioClip winSound1;
+
+    [SerializeField] AudioSource source;
+
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(gameObject);
+        else
+            _instance = this;
+    }
+
+    public void PlayWinSound()
+    {
+        source.PlayOneShot(winSound1);
+    }
+}
