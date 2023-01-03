ead2a57f1a3e434a05fc71ee03d1d0975797cba7
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2023-01-03 13:30:07 +0100

    Fixed Music bug

--- Assets/BackgroundMusic.cs
+++ Assets/BackgroundMusic.cs
@@ -5,8 +5,25 @@ using UnityEngine.Audio;
 
 public class BackgroundMusic : MonoBehaviour
 {
+    private static BackgroundMusic _instance;
+    public static BackgroundMusic Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("GameManager instance is null!");
+
+            return _instance;
+        }
+    }
+    public bool isPlaying;
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+            Destroy(gameObject);
+        else
+            _instance = this;
+
         DontDestroyOnLoad(transform.gameObject);
     }
 }
