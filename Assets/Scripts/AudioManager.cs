a1739f94735f204973e36b9365866304984c89fc
Jenny Särnberger <113012121+Hinalina@users.noreply.github.com>
2023-01-04 13:57:24 +0100

    AntiSpam Voicelines

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,6 +11,8 @@ public class AudioManager : MonoBehaviour
     public float volumeTwo = 0.2f;
     public float volumeOne = 0.1f;
 
+    private bool pressed;
+
     private static AudioManager _instance;
     public static AudioManager Instance
     {
@@ -59,28 +61,52 @@ public class AudioManager : MonoBehaviour
 
     public void RedSound()
     {
-        source.volume = volumeOne;
-        source.PlayOneShot(redSound);
+        if (pressed == false)
+        {
+            source.volume = volumeOne;
+            source.PlayOneShot(redSound);
+            StartCoroutine(AntiSpam());
+        }
     }
     public void BlueSound()
     {
-        source.volume = volumeOne;
-        source.PlayOneShot(blueSound);
+        if (pressed == false)
+        {
+            source.volume = volumeOne;
+            source.PlayOneShot(blueSound);
+            StartCoroutine(AntiSpam());
+        }
     }
     public void GreenSound()
     {
-        source.volume = volumeOne;
-        source.PlayOneShot(greenSound);
+        if (pressed == false)
+        {
+            source.volume = volumeOne;
+            source.PlayOneShot(greenSound);
+            StartCoroutine(AntiSpam());
+        }
     }
     public void PinkSound()
     {
-        source.volume = volumeOne;
-        source.PlayOneShot(pinkSound);
+        if (pressed == false)
+        {
+            source.volume = volumeOne;
+            source.PlayOneShot(pinkSound);
+            StartCoroutine(AntiSpam());
+        }
     }
 
     public void AllWin()
     {
         source.volume = volumeTwo;
         source.PlayOneShot(allWinSound1);
+    }
+
+    private IEnumerator AntiSpam()
+    {
+        pressed = true;
+        yield return new WaitForSeconds(2f);
+
+        pressed = false;
     }
 }
